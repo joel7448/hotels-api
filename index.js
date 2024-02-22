cd61026143f3f48613378a1e2a393f6beed72ae4
joel7448 <joel.joel52@gmail.com>
2024-02-22 21:22:27 +0530

    removed host

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const getDetails = require('./controller/getDetails');
 require('dotenv').config();
 const app = express();
-const host = '0.0.0.0';
+// const host = '0.0.0.0';
 
-process.env.PUPPETEER_DOWNLOAD_PATH = '/opt/render/.cache/puppeteer';
+// process.env.PUPPETEER_DOWNLOAD_PATH = '/opt/render/.cache/puppeteer';
 
 
 app.use(express.json());
@@ -12,4 +12,4 @@ app.use(express.json());
 app.get('/',getDetails);
 app.get('/:id',getDetails);
 
-app.listen( process.env.PORT || 8000 ,host, ()=>console.log('server runing...') );
+app.listen( process.env.PORT || 8000 , ()=>console.log('server runing...') );
