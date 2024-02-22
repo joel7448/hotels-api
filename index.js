3570ddb47ca75b0825eb7d93f940a11cbd13c420
joel7448 <joel.joel52@gmail.com>
2024-02-22 17:58:56 +0530

    added host

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const getDetails = require('./controller/getDetails');
 require('dotenv').config();
 const app = express();
+const host = '0.0.0.0';
 process.env.PUPPETEER_DOWNLOAD_PATH = '/opt/render/.cache/puppeteer';
 app.use(express.json());
 
 app.get('/',getDetails);
 app.get('/:id',getDetails);
 
-app.listen( process.env.PORT || 8000 ,()=>console.log('server runing...') );
+app.listen( process.env.PORT || 8000 ,host, ()=>console.log('server runing...') );
